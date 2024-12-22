f5b8b04f5ffbe24dc8c4fe12508b115f39eee721
MarkUsAMonGus12 <107293773+MarkUsAMonGus12@users.noreply.github.com>
2024-12-22 11:27:39 +0200

    Update HashPass.py
    Added a function so that hashed passwords are added to the pack and then written to the txt

--- HashPass.py
+++ HashPass.py
@@ -1,3 +1,4 @@
+import os 
 import hashlib
 print('''
       
@@ -14,27 +15,43 @@ $$/   $$/  $$$$$$$/ $$$$$$$/  $$/   $$/ $$/       $$$$$$$/ $$$$$$$/ $$$$$$$/
                     
       ''')
 print('Hi, this app will allow you to encode your password!')
-question = int(input('What you want to use? \n1. md5\n \n2. Sha256 \n \n3.Sha 225\n \n4.Sha512\n'))
+if not os.path.exists('hashed_password'):
+    os.makedirs('hashed_password')
+
+question = int(input('What you want to use? \n1. md5\n \n2. Sha256 \n \n3.Sha 224\n \n4.Sha512\n'))
 if question == 1:
     text = input('Type your password: ').encode('utf-8')
     hash = hashlib.md5(text)
     hasdxt = hash.hexdigest()
     print(hasdxt)
+    os.chdir('hashed_password')
+    with open('md5.txt', 'a') as md5:
+        md5.write(f'{text.decode('utf-8')} = {hasdxt} \n')
+    
 elif question == 2:
     text = input('Type your password: ').encode('utf-8')
     hash = hashlib.sha256(text)
     hasdxt = hash.hexdigest()
     print(hasdxt)
+    os.chdir('hashed_password')
+    with open('sha256.txt', 'a') as sha256:
+        sha256.write(f'{text.decode('utf-8')} = {hasdxt} \n')
 elif question == 3:
     text = input('Type your password: ').encode('utf-8')
-    hash = hashlib.sha225(text)
+    hash = hashlib.sha224(text)
     hasdxt = hash.hexdigest()
     print(hasdxt)
+    os.chdir('hashed_password')
+    with open('sha225.txt', 'a') as sha224:
+        sha224.write(f'{text.decode("utf-8")} = {hasdxt} \n')
 elif question == 4:
     text = input('Type your password: ').encode('utf-8')
     hash = hashlib.sha512(text)
     hasdxt = hash.hexdigest()
     print(hasdxt)
+    os.chdir('hashed_password')
+    with open('sha512.txt', 'a') as sha512:
+        sha512.write(f'{text.decode('utf-8')} = {hasdxt} \n')
 else:
     print('Error')
     
